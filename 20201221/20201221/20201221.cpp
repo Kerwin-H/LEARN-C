4cb1f2a66631a3ed86880ca9254b9a48dd88cb25
kerwin-628 <648017848@qq.com>
2020-12-21 22:15:12 +0800

    考试周,复习以前的代码(1)

---
+++ 20201221/20201221/20201221.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<string.h>
+int find(char arr[],char *p){
+	int n = strlen(arr);
+	int m = strlen(p);
+	if (m != n){ return 0; }
+	strncat(arr, arr, n);
+	if (strstr(arr, p) == NULL){ return 0; }
+	else
+		return 1; 
+}
+int main(){
+	char arr[20] = "AABCD";
+	char *p = "BCDAA";
+	int ret = find(arr, p);
+	if (ret == 1){
+		printf("1\n");
+	}
+	else if (ret==0){
+		printf("0\n");
+	}
+	system("pause");
+	return 0;
+}
