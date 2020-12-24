a2e081b69fe124e7fb40fbcfca7b46a04eed9f58
kerwin-628 <648017848@qq.com>
2020-12-24 21:46:22 +0800

    strstr代码复

---
+++ 20201224/20201224/20201224.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+char *my_strstr(const char* dest,const char* src){
+	char* start = (char*)dest;
+	char* substart = (char*)src;
+	char* cp = (char*)dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+	while (*cp){
+		start = cp;
+		while (*start!='\0'&&*substart!='\0'&&*start==*substart){
+			start++;
+			substart++;
+		}
+		if (*substart=='\0'){
+			return cp;
+		}
+		substart = (char*)src;
+		cp++;
+	}
+	return NULL;
+}
+int main(){
+	char arr[20] = "aabbccede";
+	char arr1[10] = "bcce";
+
+	printf("%s\n",my_strstr(arr,arr1));
+
+	system("pause");
+	return 0;
+}
