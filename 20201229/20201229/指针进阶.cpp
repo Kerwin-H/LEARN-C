0e4b09eadcba8968621bb42552d0c7c508dfa696
kerwin-628 <648017848@qq.com>
2020-12-29 12:44:35 +0800

    1110录屏结

---
+++ 20201229/20201229/指针进阶.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void print(int(*p)[5]){
+	
+}
+void print(int arr[3][5]){
+
+}
+int main(){
+
+
+	//char a = 'a';
+	//char* p = &a;
+
+	//char name[1024] = "hello";  //指向字符串
+	//char* p2 = name;	//p2表示其name首元素地址
+
+	//int* arr[4] = { 0 };  //指针数组
+
+	//int arr2[4] = { 0 };  //数组指针
+	//int(*p)[4] = &arr2;
+
+	//printf("%p\n", arr2 );
+	//printf("%p\n",arr2+1);
+	//printf("%p\n", &arr2+1);
+
+	int arr[3][5] = { 0 };
+	print();
+
+
+
+
+
+	system("pause");
+	return 0;
+}
