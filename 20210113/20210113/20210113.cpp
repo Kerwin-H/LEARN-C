b84c43aa91a97a0fa441da43782c6c0121dfe3a0
kerwin-628 <648017848@qq.com>
2021-01-14 23:33:09 +0800

    复 11.21 录屏 主要了解 位段 枚举 联合体等

--- 20210113/20210113/20210113.cpp
+++ 20210113/20210113/20210113.cpp
@@ -4,94 +4,99 @@
 #include<string.h>
 #include<assert.h>
 
-
-//内存拷贝
-//!!!!!!!!!!注意参数的合法性校验!!!!!! 
-void* myMemcpy(void* dest ,const void* src,size_t num){
-	assert(dest != NULL&&src != NULL);
-	char* pDest = (char*)dest;
-	char* pSrc = (char*)src;
-	for (int i = 0; i < num; i++){
-		*(pDest + i) = *(pSrc + i);
-	}
-	return dest;
-}
-
-//当内存发生重叠的时候,memove是正确的,memcpy不确定
-void* myMemove(void* dest, const void* src, size_t num){
-	assert(dest != NULL&&src != NULL);
-	char* pDest = (char*)dest; 
-	char* pSrc = (char*)src;
-	if (pSrc <= pDest && pDest <= pSrc + num){
-		for (int i = num - 1; i >= 0; i--){
-			*(pDest + i) = *(pSrc + i);
-		}
-	}
-	else{
-		return myMemcpy(dest, src, num);
-	}
-}
-
-
-
-int main(){
-	
-	//查找切分,找寻所要找的东西,将其切换为空格(有缺陷)
-	/*char str[] = "I am a student";
-	char* p = strtok(str," ");
-	while (p!=NULL){
-		printf("%s\n",p);
-		p = strtok(NULL, " ");
-	}*/
-
-	//memcpy
-	//int arr[4] = { 1, 2, 3, 4 };
-	//int arr2[4] = { 0 };
-	//memcpy(arr2, arr, 16);//==sizeof(arr2);
-	//for (int i = 0; i < 4; i++)
-	//printf("%d ",arr2[i]);  //在这里要注意到数组需要用循环才能输出
-
-	system("pause");
-	return 0;
-}
-
-
-//====第四节
-struct Student{
-	char name[1024];
-	int age;
-
-};
-
-//不可包含自己的结构体成员
-struct School{
-	
-	struct Student students[1024];
-	
-	struct School* school;//加* 可以,指针
-};
+//
+////内存拷贝
+////!!!!!!!!!!注意参数的合法性校验!!!!!! 
+//void* myMemcpy(void* dest ,const void* src,size_t num){
+//	assert(dest != NULL&&src != NULL);
+//	char* pDest = (char*)dest;
+//	char* pSrc = (char*)src;
+//	for (int i = 0; i < num; i++){
+//		*(pDest + i) = *(pSrc + i);
+//	}
+//	return dest;
+//}
+//
+////当内存发生重叠的时候,memove是正确的,memcpy不确定
+//void* myMemove(void* dest, const void* src, size_t num){
+//	assert(dest != NULL&&src != NULL);
+//	char* pDest = (char*)dest; 
+//	char* pSrc = (char*)src;
+//	if (pSrc <= pDest && pDest <= pSrc + num){
+//		for (int i = num - 1; i >= 0; i--){
+//			*(pDest + i) = *(pSrc + i);
+//		}
+//	}
+//	else{
+//		return myMemcpy(dest, src, num);
+//	}
+//}
+//
+//
+//
+//int main(){
+//	
+//	//查找切分,找寻所要找的东西,将其切换为空格(有缺陷)
+//	/*char str[] = "I am a student";
+//	char* p = strtok(str," ");
+//	while (p!=NULL){
+//		printf("%s\n",p);
+//		p = strtok(NULL, " ");
+//	}*/
+//
+//	//memcpy
+//	//int arr[4] = { 1, 2, 3, 4 };
+//	//int arr2[4] = { 0 };
+//	//memcpy(arr2, arr, 16);//==sizeof(arr2);
+//	//for (int i = 0; i < 4; i++)
+//	//printf("%d ",arr2[i]);  //在这里要注意到数组需要用循环才能输出
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//
+////====第四节
+//struct Student{
+//	char name[1024];
+//	int age;
+//
+//};
+//
+////不可包含自己的结构体成员
+//struct School{
+//	
+//	struct Student students[1024];
+//	
+//	struct School* school;//加* 可以,指针
+//};
 
 //====!!!内存对齐规则!!!(在这里就是把不同字节数储存在里面,看对内存的应用)
 
-#pragma pack(1)
+//#pragma pack(1)
 //代码跨平台
 typedef struct A{
-	char one;
-	short two;
-	int three;
+	char name;  
+	int age;  
+	short b;     
+	long d; 
+	char sex;	
+	short e; 
 }A;
 //还原代码
-#pragma pack()
+//#pragma pack()
 
 int main(){
 	A a;
 	printf("%d\n",sizeof(A));
-	printf("%p\n",&a);
-	printf("%p\n",&a.one);
-	printf("%p\n",&a.two);
-	printf("%p\n",&a.three);
-
-
+	/*printf("%p\n",&a.name);
+	printf("%p\n", &a.age);
+	printf("%p\n", &a.b);
+	printf("%p\n", &a.d);
+	printf("%p\n", &a.sex);
+	printf("%p\n", &a.e);
+	printf("%p\n", &a.f);
+*/
 
 	system("pause");
 	return 0;
---
+++ 20210114/20210114/20210114.cpp
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+struct A{
+	int _a : 2;
+	int _b : 5;
+	int _c : 10;
+	int _d : 30;
+};
+
+//创建了一个新的枚举的数据类型
+enum Sex{
+	MALE=100,     //不能当成浮点数
+	FEMALE,
+	UNKNOWN,
+};
+
+typedef enum Sex Sex;
+
+//创建一个联合体
+union Test{
+	char a;
+	short b;
+	int c;
+};
+
+
+//大小端字节序判断
+int isBigEnd(){
+	int a = 0x11223344;
+	int* p = &a;
+	char* p2 = (char*)p;
+	if (*p2 == 0x11){
+		return 1;
+	}
+	return 0;
+}
+//联合体实现!!!!!
+int isBigEnd2(){
+	union Un{
+		char a;
+		int b;
+	}u;
+	u.b = 0x11223344;
+	if (u.a == 0x11){
+		return 1;
+	}
+	return 0;
+}
+
+
+int main(){
+
+	int arr[4];
+	memset(arr, 0, 16);  //相当于初始化
+	
+	printf("%d\n",sizeof(struct A));
+//==============================================
+	//创建一个枚举类型的变量
+	enum Sex s = MALE;  //或者进行别名的的定义
+
+	printf("%d\n",MALE);
+	printf("%d\n",FEMALE);
+	printf("%d\n",UNKNOWN);
+
+
+	//联合体使用场景少,牵制太多
+	union Test t;
+	t.c = 0x11223344;
+	printf("%x\n",t.c);
+	t.a = 0x55;
+	printf("%x\n",t.c);
+	//printf("%d\n",sizeof(t));
+
+
+	system("pause");
+	return 0;
+}
